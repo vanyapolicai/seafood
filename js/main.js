dbd6b908810ca675b33486ca78b37e9f0be05cf6
vanyapolicai <113535896+vanyapolicai@users.noreply.github.com>
2022-10-20 11:27:50 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function () {
+  $('.header__slider').slick({
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/prev.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
+    vertical: true,
+  });
+  $('.product__name').slick({
+	slidesToShow: 6,
+	slidesToScroll: 1,
+	focusOnSelect: true,
+	asNavFor: '.product__content',
+	vertical: true,
+	prevArrow: '<button type="button" class="product-prev"><img src="images/prod-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="product-next"><img src="images/prod-next.svg" alt=""></button>',
+  });
+  $('.product__content').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	asNavFor: '.product__name',
+	fade: true,
+	arrows: false,
+  });
+});
